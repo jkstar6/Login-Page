a12baa79887a36fdde03010cb112cf2598fb37ce
Bintang Jati <bintangid69@gmail.com>
2025-05-17 00:43:42 +0700

    add javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+const passwordInput = document.getElementById('passwordInput')
+const toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click', function() {
+    const isPassword = passwordInput.type === 'password'
+    passwordInput.type = isPassword ? 'text' : 'password'
+
+    toggle.src = isPassword ? 'img/icons8-closed-eye-50.png' : 'img/icons8-eye-50.png'
+})
